5db734725fb2122a08dff0a9f31e2aa2cb385829
almonasterio <a.monasterio.a@gmail.com>
2020-10-12 11:26:42 -0500

    fixed components

--- src/Main.js
+++ src/Main.js
@@ -3,18 +3,16 @@ import React from 'react';
 import 'font-awesome/css/font-awesome.min.css';
 
 import MainLeft from './components/mainLeft/MainLeft';
-import Blogs from './components/mainRight/Blogs';
 
 const Main = () => {
 
     return (
         <div className="relative md:flex ">
             <MainLeft />
-            {/* <Blogs /> */}
+  
          
 
         </div>
-
 
 
 
