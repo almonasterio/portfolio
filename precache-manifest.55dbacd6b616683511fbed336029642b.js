6dbfd38d9a1c58367224a18aae40cd52e620db09
almonasterio <a.monasterio.a@gmail.com>
2020-10-12 07:43:17 -0500

    Updates

---
+++ precache-manifest.55dbacd6b616683511fbed336029642b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb02460536743873ad0f6ac48ea5c0c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1ba6f03e4c00141aaa0b",
+    "url": "/portfolio/static/css/2.3d60f3ae.chunk.css"
+  },
+  {
+    "revision": "c2d1f64a6d0e4f3d2fbf",
+    "url": "/portfolio/static/css/main.aca219f1.chunk.css"
+  },
+  {
+    "revision": "1ba6f03e4c00141aaa0b",
+    "url": "/portfolio/static/js/2.2a16dda2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.2a16dda2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2d1f64a6d0e4f3d2fbf",
+    "url": "/portfolio/static/js/main.279ab54c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "9d6480c3353f76e668420baee81e8a4d",
+    "url": "/portfolio/static/media/Alvaro.Monasterio.Resume.9d6480c3.pdf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
